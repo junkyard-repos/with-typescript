3779d4fdd59a3529313d5df5cd2fc7561155fd51
David Long <davdaarn@live.com>
2021-06-13 12:58:51 -0600

    stuff

---
+++ src/views/Test.vue
@@ -0,0 +1,39 @@
+<template>
+	<div>
+		<h1>{{}}</h1>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { User } from "../types";
+
+export default Vue.extend({
+	name: "Home",
+	data() {
+		return {
+			user: {} as User,
+		};
+	},
+	computed: {
+		fullName(): string {
+			return `${this.user.firstName} ${this.user.lastName}`;
+		},
+	},
+	mounted(): void {
+		// this.user = {
+		// 	firstName: `Sammy`,
+		// 	lastName: `Shark`,
+		// 	twitterHandle: `@digitalocean`,
+		// 	location: {
+		// 		city: `New York City`,
+		// 		state: `NY`,
+		// 	},
+		// };
+	},
+});
+</script>
+
+
+<style>
+</style>
